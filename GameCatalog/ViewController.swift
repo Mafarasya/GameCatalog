2062361a49b55dafc3658a486f1a04338ef4902a
Mahatmaditya Favian Rachman Syarief <mahatmadityafrs88@gmail.com>
2024-05-10 23:01:12 +0700

    Initial Commit

---
+++ GameCatalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameCatalog
+//
+//  Created by Mahatmaditya FRS on 10/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
